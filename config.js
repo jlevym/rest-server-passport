eed513806e97c51349ec360631837a9272a73930
Jeffrey <jlevym@gmail.com>
2016-03-31 17:42:16 +0300

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+    	clientID: '926826740747824',
+    	clientSecret: '32e3fc8932736b858a14784769f55df2',
+    	callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
